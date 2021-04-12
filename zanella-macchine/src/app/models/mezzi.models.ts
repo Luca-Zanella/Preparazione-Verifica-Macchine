a592c79b29c5f57265c27197e73f299f84e691b7
Zanella Luca <zanella.luca@iisgalvanimi.edu.it>
2021-04-12 10:24:21 +0200

    punto 1

---
+++ zanella-macchine/src/app/models/mezzi.models.ts
@@ -0,0 +1,17 @@
+export class Mezzo{
+
+    tipo:string;
+    descrizione:string;
+    tariffa:string;
+    valutazionemedia:string;
+
+
+    constructor(tipo:string,descrizione:string,tariffa:string,valutazionemedia:string){
+        this.tipo = tipo
+        this.descrizione = descrizione
+        this.tariffa = tariffa
+        this.valutazionemedia = valutazionemedia
+    }
+    
+
+}
