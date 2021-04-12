2a749f8b9530efc9b84f151355e177ddf7ff8b3a
Zanella Luca <zanella.luca@iisgalvanimi.edu.it>
2021-04-12 10:46:24 +0200

    Punto 2

--- zanella-macchine/src/app/app.component.ts
+++ zanella-macchine/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Mezzo } from './models/mezzi.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'zanella-macchine';
+export class AppComponent implements OnInit {
+
+  obsMezzi:Observable<Mezzo[]>
+  //varabile che conterrà mezzo
+  listaMezzi:Mezzo[];
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    //ricordare che l'obs deve ritornare un vettore di tipo della classe creata
+    this.obsMezzi = this.http.get<Mezzo[]>("https://my-json-server.typicode.com/malizia-g/fine_anno_exp/mezzi")
+    this.obsMezzi.subscribe(this.getMezzi)
+  }
+
+  getMezzi = (data : Mezzo[]) =>{
+    //lista mezzi conterrà tutto grazie all'arrow function
+    this.listaMezzi = data
+    console.log(this.listaMezzi)
+  }
 }
---
+++ zanella-macchine/src/app/driving-list/driving-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Mezzo } from '../models/mezzi.models';
+
+@Component({
+  selector: 'app-driving-list',
+  templateUrl: './driving-list.component.html',
+  styleUrls: ['./driving-list.component.css'],
+})
+export class DrivingListComponent implements OnInit {
+  @Input() listaMezzi: Mezzo[];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
